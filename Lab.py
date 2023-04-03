3c810c2ba9d4998dc8447cd29262ea715abab7cf
Gabriel Lopes <57074487+unifgabsantos@users.noreply.github.com>
2023-04-03 20:22:33 -0300

    Update Lab.py

--- Lab.py
+++ Lab.py
@@ -5,15 +5,16 @@ for line in file.readlines():
         line = line.split('"')
         fraseMD5 = hashlib.md5(line[1].encode("utf-8")).hexdigest()
         fraseSHA256 = hashlib.sha256(line[1].encode("utf-8")).hexdigest()
+        frase = line[1]
         line = line[2].split("-")
         sha256 = line[1]
         md5 = line[2].strip()
         if(md5==fraseMD5 and sha256==fraseSHA256):
-           print("SHA256 e MD5 corretos")
+           print(f"{frase} | SHA256 e MD5 corretos")
         elif(md5==fraseMD5 and sha256!=fraseSHA256):
-            print("Somente SHA256 correto")
+            print(f"{frase} | Somente SHA256 correto")
         elif(md5!=fraseMD5 and sha256==fraseSHA256):
-            print("Somente MD5 correto")
+            print(f"{frase} | Somente MD5 correto")
         else:
-            print("Nenhum dos dois corretos")
+            print(f"{frase} | Nenhum dos dois corretos")
 file.close()
